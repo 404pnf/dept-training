ca63e974db58b1c03cb0df3fd22e5c736ea1f0bd
404pnf <404pnf@exmaple.org>
2014-06-04 11:36:26 +0800

    服务器上测试成功

--- sinatra-as-mysql-proxy/server.rb
+++ sinatra-as-mysql-proxy/server.rb
@@ -2,26 +2,22 @@ require 'sinatra'
 require 'mysql2'
 require 'json'
 
-client = Mysql2::Client.new(
-  host: 'localhost',
-  database: 'iyc_whx',
-  username: 'py',
-  password: 'pengyou'
-  )
+set :bind, '129.0.0.47'
 
 client = Mysql2::Client.new(
-  host: '129.0.0.47',
+  host: 'localhost',
+  # host: '129.0.0.47',
   database: 'iyc_whx',
   username: 'py',
   password: 'pengyou'
   )
 
 results = client.query("SELECT * FROM rank WHERE sales>10")
-
-json = results.to_json
+json = results.to_a.to_json # 必须先转为数组，不能直接转到json
+                            # result的类是Mysql2的
 
 get '/' do
   sleep 2
-  json
+  "#{json}"
 end
 
